fix and improve items masking logic in collate

src/concept/data/collate.py
@@ -159,6 +159,14 @@ class Collate(BaseCollate):
         mask = mask_1 & mask_2
         return mask
 
+    def _apply_items_mask(self, items_mask, batch_1, batch_2, seq_length_1, seq_length_2):
+        for i in range(len(batch_1)):
+            if not items_mask[i]:
+                batch_1[i] = {"tokens": np.array([]), "values": np.array([])}
+                batch_2[i] = {"tokens": np.array([]), "values": np.array([])}
+                seq_length_1[i] = 0
+                seq_length_2[i] = 0
+
     def _get_predesigned_panel(self, batch, organism, tissues):
         # Randomly select a panel from that organism
         panels = self.panels_dict[organism]
@@ -245,6 +253,12 @@ class Collate(BaseCollate):
             seq_length_2 = [min(len(item["tokens"]), self.max_tokens) for item in batch_2]
 
             items_mask = np.ones(len(batch_1), dtype=bool)
+
+            if self.stage == "train" and self.qc_threshold is not None:
+                qc_mask = self.qc_mask(seq_length_1, seq_length_2, panel_1, panel_2)
+                items_mask &= qc_mask
+                self._apply_items_mask(items_mask, batch_1, batch_2, seq_length_1, seq_length_2)
+
             if (
                 self.stage == "train"
                 and self.max_total_seq_length is not None
@@ -253,17 +267,7 @@ class Collate(BaseCollate):
                 batch_size = self.adapt_batch_size(seq_length_1, seq_length_2)
                 if batch_size < len(batch_1):
                     items_mask[batch_size:] = False
-
-            if self.stage == "train" and self.qc_threshold is not None:
-                qc_mask = self.qc_mask(seq_length_1, seq_length_2, panel_1, panel_2)
-                items_mask &= qc_mask
-
-            for i in range(len(batch_1)):
-                if not items_mask[i]:
-                    batch_1[i] = {"tokens": np.array([]), "values": np.array([])}
-                    batch_2[i] = {"tokens": np.array([]), "values": np.array([])}
-                    seq_length_1[i] = 0
-                    seq_length_2[i] = 0
+                    self._apply_items_mask(items_mask, batch_1, batch_2, seq_length_1, seq_length_2)
 
             max_lenght_1 = max(seq_length_1)
             max_lenght_2 = max(seq_length_2)